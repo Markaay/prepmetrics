b517f651394469c4833baadbce99a2c632b4a89c
Mark <Ebergen@MacBook-Air-van-Mark.local>
2017-12-12 20:15:56 +0100

    tablenameincludes

--- prepModules/prepfacebookowned.py
+++ prepModules/prepfacebookowned.py
@@ -59,7 +59,7 @@ def fb_ownedpublicapmmetrics(app, pagedata, contextdata, connectiondata):
         "page_new_talks": 0
     }
     querydata ={
-        "table": connectiondata["page_table"],
+        "table": connectiondata["db_name"]+"."+connectiondata["page_table"],
         "starttime": contextdata["yesterday_date"][:10] +" 00:00:00",
         "endtime": contextdata["yesterday_date"][:10] +" 23:59:59"
     }
@@ -69,7 +69,8 @@ def fb_ownedpublicapmmetrics(app, pagedata, contextdata, connectiondata):
     con = mdb.connect(connectiondata["con_ip"], connectiondata["con_user"], connectiondata["con_pass"], connectiondata["con_db"])
     with con:
         cur = con.cursor()
-        query = ("SELECT page_name, page_id, page_fan_count, page_were_here_count, page_talking_about_count FROM %(table)s WHERE scrape_date BETWEEN %(starttime)s AND %(endtime)s ")
+        query = ("SELECT page_name, page_id, page_fan_count, page_were_here_count, page_talking_about_count FROM %(table)s "
+                 "WHERE scrape_date BETWEEN %(starttime)s AND %(endtime)s ")
         print(query)         
         #execute query with context data
         que = cur.execute(query, querydata)
