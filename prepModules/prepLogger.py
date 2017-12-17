67e23df3c0e0238cb6c7b37163c8f9bd0aecb1f7
Mark <Ebergen@MacBook-Air-van-Mark.local>
2017-12-17 21:32:57 +0100

    2

--- prepModules/prepLogger.py
+++ prepModules/prepLogger.py
@@ -1,13 +1,13 @@
 """creates a custom logger for specific apps"""
 
 #Global log configuration
-def applogger(name):
-    """creates a custom logger to debug functions and apps"""
-    import logging
-    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
-    handler = logging.FileHandler(name+'.log')
-    handler.setFormatter(formatter)
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(handler)
-    return logger
+#def applogger(name):
+#    """creates a custom logger to debug functions and apps"""
+#    import logging
+#    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+#    handler = logging.FileHandler(name+'.log')
+#    handler.setFormatter(formatter)
+#    logger = logging.getLogger(name)
+#    logger.setLevel(logging.DEBUG)
+#    logger.addHandler(handler)
+#    return logger
